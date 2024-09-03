735ce60239be44ea9b027ff6369d7a664e81cad2
HarukiKondo <haruki-kondo@secom.co.jp>
2024-09-03 06:16:16 +0000

    更新

---
+++ packages/contract/scripts/run.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+
+/**
+ * テスト用サンプルスクリプト
+ */
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy();
+  const nftGame = await gameContract.deployed();
+
+  console.log("Contract deployed to:", nftGame.address);
+};
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
