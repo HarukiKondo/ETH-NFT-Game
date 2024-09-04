1d0df051da8e8049c32c3d941c1b4db395098e9e
HarukiKondo <haruki-kondo@secom.co.jp>
2024-09-04 14:23:10 +0900

    更新

---
+++ packages/client/src/Components/SelectCharacter/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import "./SelectCharacter.css";
+
+/**
+ *  SelectCharacter コンポーネント
+ * @param {*} param0
+ * @returns
+ */
+const SelectCharacter = ({ setCharacterNFT }) => {
+	return (
+		<div className="select-character-container">
+			<h2>⏬ 一緒に戦う NFT キャラクターを選択 ⏬</h2>
+		</div>
+	);
+};
+export default SelectCharacter;
