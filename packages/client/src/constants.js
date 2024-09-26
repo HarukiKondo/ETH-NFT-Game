0d4d0c6a059036df2b102cf8f18f8f331dc6bf0d
HarukiKondo <haruki-kondo@secom.co.jp>
2024-09-26 01:26:09 +0000

    プッシュ

---
+++ packages/client/src/constants.js
@@ -0,0 +1,15 @@
+const CONTRACT_ADDRESS = "YOUR_CONTRACT_ADDRESS_GOES_HERE";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
